1fe310df373f7ef34059ce50e97ad8a4b02879c4
Likhithakanavu <likhithakanavu@gmail.com>
2023-07-17 11:25:29 +0530

    backend first commit

--- routers/product.js
+++ routers/product.js
@@ -1,11 +1,19 @@
 const express = require("express")
 const router = express.Router()
 const Product = require('../model/product')
+const { InsertProduct, ViewProduct, DeleteProduct, UpdateProduct } = require('../controller/product')
 
-
-router.get("/get",(req,res)=>{
-    res.json({message:"hello"})
+// router.get("/get",(req,res)=>{
+//     res.json({message:"hello"})
     
-})
+// })
+
+router.post('/insert', InsertProduct )
+
+router.get('/view', ViewProduct )
+
+router.delete('/delete', DeleteProduct )
+
+router.put('/update', UpdateProduct )
 
 module.exports = router
