d622962bcff193e5e7b863da6247076f8c29a734
nihad313 <mohammedniddu456@gmail.com>
2023-07-20 11:15:09 +0530

    Changed

--- routers/product.js
+++ routers/product.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 const Product = require('../model/product')
 const { InsertProduct, ViewProduct, DeleteProduct, UpdateProduct } = require('../controller/product')
-
+const {InsertParty,ViewParty,UpdateParty} = require('../controller/party')
 // router.get("/get",(req,res)=>{
 //     res.json({message:"hello"})
     
@@ -17,5 +17,14 @@ router.get('/view/:id', ViewProduct )
 router.delete('/delete/:id', DeleteProduct )
 
 router.put('/update/:id', UpdateProduct )
+
+// //party
+router.post('/party_insert',InsertParty)
+
+router.get('/view_party',ViewParty)
+
+router.get('/view_party/:id',ViewParty)
+
+router.put('/party_update/:id',UpdateParty)
 
 module.exports = router
